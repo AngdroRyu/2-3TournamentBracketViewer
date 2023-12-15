7d548185d9ead3a8a55563b83364d00f606f096b
Emily Mc Nett <61562565+Emily-McNett@users.noreply.github.com>
2023-12-15 13:59:22 -0600

    Fixed 'final result' bug, separated match 8 out, and added minor formatting.

--- DemoFiles/JaydenDemo/smartpage.php
+++ DemoFiles/JaydenDemo/smartpage.php
@@ -4,14 +4,16 @@ $username = "user23";
 $password = "roof";
 $dbname = "AgileExpG23";
 
-#echo '<link rel="stylesheet" type="text/css" href="mystyle.css">';
-#Tommy and Emily
+#php to show the matchView of each match - Emily and Tommy
+
+echo '<link rel="stylesheet" type="text/css" href="mystyle.css">';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error){
 	die("connection failed" . $conn->connect_error);
 }
 
+#switch case to change quieries based on match selected
 $matchType = $_GET['content']; 
     switch ($matchType) {
         case "content_1":
@@ -21,8 +23,8 @@ $matchType = $_GET['content'];
 		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='2'";
 		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='1'";
 		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='1'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='1'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='1'";
+		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Won', 'Lost') AS 'final1' FROM AgileExpG23.Match WHERE id='1'";
+		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Won', 'Lost') AS 'final2' FROM AgileExpG23.Match WHERE id='1'";
 		break;
 	case "content_2":
 		$sql_teamName1 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='3'";
@@ -31,8 +33,8 @@ $matchType = $_GET['content'];
 		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='4'";
 		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='2'";
 		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='2'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='2'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='2'";
+		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Won', 'Lost') AS 'final1' FROM AgileExpG23.Match WHERE id='2'";
+		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Won', 'Lost') AS 'final2' FROM AgileExpG23.Match WHERE id='2'";
 		break;
 	case "content_3":
 		$sql_teamName1 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='5'";
@@ -41,8 +43,8 @@ $matchType = $_GET['content'];
 		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='6'";
 		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='3'";
 		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='3'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='3'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='3'";
+		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Won', 'Lost') AS 'final1' FROM AgileExpG23.Match WHERE id='3'";
+		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Won', 'Lost') AS 'final2' FROM AgileExpG23.Match WHERE id='3'";
 		break;
 	case "content_4":
 		$sql_teamName1 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='7'";
@@ -51,8 +53,8 @@ $matchType = $_GET['content'];
 		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='8'";
 		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='4'";
 		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='4'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='4'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='4'";
+		$sql_team1Final = "SELECT IIF(team1Score>team2Score, 'Won', 'Lost') AS 'final1' FROM AgileExpG23.Match WHERE id='4'";
+		$sql_team2Final = "SELECT IIF(team1Score<team2Score, 'Won', 'Lost') AS 'final2' FROM AgileExpG23.Match WHERE id='4'";
 		break;
 	case "content_5":
 		$sql_teamName1 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='2'";
@@ -61,8 +63,8 @@ $matchType = $_GET['content'];
 		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='3'";
 		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='5'";
 		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='5'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='5'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='5'";
+		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Won', 'Lost') AS 'final1' FROM AgileExpG23.Match WHERE id='5'";
+		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Won', 'Lost') AS 'final2' FROM AgileExpG23.Match WHERE id='5'";
 		break;
 	case "content_6":
 		$sql_teamName1 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='6'";
@@ -71,8 +73,8 @@ $matchType = $_GET['content'];
 		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='7'";
 		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='6'";
 		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='6'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='6'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='6'";
+		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Won', 'Lost') AS 'final1' FROM AgileExpG23.Match WHERE id='6'";
+		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Won', 'Lost') AS 'final2' FROM AgileExpG23.Match WHERE id='6'";
 		break;
 	case "content_7":
 		$sql_teamName1 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='2'";
@@ -81,21 +83,12 @@ $matchType = $_GET['content'];
 		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='6'";
 		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='7'";
 		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='7'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='7'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='7'";
-		break;
-	case "content_8":
-		$sql_teamName1 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='1'";
-		$sql_teamName2 = "SELECT Team.name FROM AgileExpG23.Team WHERE Team.id='2'";
-		$sql_playerList1 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='1'";
-		$sql_playerList2 = "SELECT Player.firstName, Player.lastName FROM AgileExpG23.Player WHERE Player.team='2'";
-		$sql_team1Score = "SELECT team1Score FROM AgileExpG23.Match WHERE id='7'";
-		$sql_team2Score = "SELECT team2Score FROM AgileExpG23.Match WHERE id='7'";
-		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Winner', 'Loser') FROM AgileExpG23.Match WHERE id='7'";
-		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Loser', 'Winner') FROM AgileExpG23.Match WHERE id='7'";
+		$sql_team1Final = "SELECT IF(team1Score>team2Score, 'Won', 'Lost') AS 'final1' FROM AgileExpG23.Match WHERE id='7'";
+		$sql_team2Final = "SELECT IF(team1Score<team2Score, 'Won', 'Lost') AS 'final2' FROM AgileExpG23.Match WHERE id='7'";
 		break;
     } 
 
+#gather results of the queries 
 $result_playerList1 = $conn->query($sql_playerList1);
 $result_playerList2 = $conn->query($sql_playerList2);
 $result_teamName1 = $conn->query($sql_teamName1);
@@ -105,60 +98,71 @@ $result_team2Score = $conn->query($sql_team2Score);
 $result_team1Final = $conn->query($sql_team1Final);
 $result_team2Final = $conn->query($sql_team2Final);
 
+#create and fill table for team 1
 $team1_table = '<table border="0" cellspacing="0" cellpadding="2">';
 foreach($result_teamName1 as $row) {
-	$team1_table .= '<tr><td>' .$row['name']. '</td></tr>';
+	$team1_table .= '<tr><td><b><u>' .$row['name']. '</u></b></td></tr>';
 }
+
+$team1_table .= '<tr><td><b> Team Results: </b></td>';
 
 foreach($result_team1Final as $row) {
-	$team1_table .= '<tr><td>' .$row['IF(team1Score>team2Score, "Winner", "Loser")']. '</td></tr>';
+	$team1_table .= '<td>' .$row['final1']. '</td>';
 }
 
-$team1_table .= '<tr><td><b> Score: </b></td></tr>';
+$team1_table .= '</tr>';
+
+$team1_table .= '<tr><td><b> Team Score: </b></td>';
 foreach($result_team1Score as $row) {
-	$team1_table .= '<tr><td>' .$row['team1Score']. '</td></tr>';
+	$team1_table .= '<td>' .$row['team1Score']. '</td>';
 }
+
+$team1_table .= '</tr>';
 
 $team1_table .= '<tr><td><b> Players List: </b></td></tr>';
-$team1_table .= '<tr><td><b> First Name </b></td><td><b> Last Name </b></td></tr>';
+$team1_table .= '<tr><td><i> First Name </i></td><td><i> Last Name </i></td></tr>';
 
 foreach($result_playerList1 as $row) {
 	$team1_table .= '<tr><td>' .$row['firstName']. '</td><td>' .$row['lastName']. '</td></tr>';
 }
 
-//team 2 table
+$team1_table .= '</table>';
+
+#create and fill table for team 2
 $team2_table = '<table border="0" cellspacing="0" cellpadding="2">';
 foreach($result_teamName2 as $row) {
-	$team2_table .= '<tr><td>' .$row['name']. '</td></tr>';
+	$team2_table .= '<tr><td><b><u>' .$row['name']. '</u></b></td></tr>';
 }
+
+$team2_table .= '<tr><td><b> Team Results: </b></td>';
 
 foreach($result_team2Final as $row) {
-	$team2_table .= '<tr><td>' .$row['IF(team1Score>team2Score, "Loser", "Winner")']. '</td></tr>';
+	$team2_table .= '<td>' .$row['final2']. '</td>';
 }
 
-$team2_table .= '<tr><td><b> Score: </b></td></tr>';
+$team2_table .= '</tr>';
 
+$team2_table .= '<tr><td><b> Team Score: </b></td>';
 foreach($result_team2Score as $row) {
-	$team2_table .= '<tr><td>' .$row['team2Score']. '</td></tr>';
+	$team2_table .= '<td>' .$row['team2Score']. '</td>';
 }
+
+$team2_table .= '</tr>';
 
 $team2_table .= '<tr><td><b> Players List: </b></td></tr>';
-$team2_table .= '<tr><td><b> First Name </b></td><td><b> Last Name </b></td></tr>';
+$team2_table .= '<tr><td><i> First Name </i></td><td><i> Last Name </i></td></tr>';
 
 foreach($result_playerList2 as $row) {
 	$team2_table .= '<tr><td>' .$row['firstName']. '</td><td>' .$row['lastName']. '</td></tr>';
 }
 
+$team2_table .= '</table>';
 
-
-
-$team1_table .= '</table>';
-
+#echo both tables to matchView html
 echo $team1_table;
 echo $team2_table;
 
-#header("Location:matchView.html");
-
+#end connection with database
 $conn = null;
 
 ?>
